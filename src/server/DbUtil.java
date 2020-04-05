ffd37a893be5694ab32c3b18baf4e4415db318c2
Qie-Dan <13799658498@163.com>
2020-04-05 20:03:55 +0800

    update DbUtil

--- src/server/DbUtil.java
+++ src/server/DbUtil.java
@@ -1,8 +1,6 @@
 package server;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 /**
  * 数据库的通用工具类
@@ -33,11 +31,31 @@ public class DbUtil {
      * @param account_name 昵称
      * @param account_password 密码
      */
-    public static void addAccount(
+    public static void createAccount(
             String account_id, String account_name, String account_password) throws SQLException {
 
         // 建立数据库连接
         Connection conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+
+        // 具体的命令
+        String sql = "insert into accounts(account_id,account_name,account_password)" +
+                " values(?,?,?) ";
+
+        // PreparedStatement对象可以防止sql注入
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ps.setString(1, account_id);
+        ps.setString(2, account_name);
+        ps.setString(3, account_password);
+
+        // executeUpdate()函数执行增删改操作，返回更新的行数
+        int rowsChanged = ps.executeUpdate();
+        if(rowsChanged == 1) {
+            System.out.println("添加成功");
+        }
+
+        // 关闭连接，释放资源
+        conn.close();
+        ps.close();
     }
 
 }
