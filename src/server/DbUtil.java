332bbeb9a5361a293727a600b50b63f55cf400b3
Qie-Dan <13799658498@163.com>
2020-04-06 08:41:17 +0800

    update DbUtil:update idMatch()

--- src/server/DbUtil.java
+++ src/server/DbUtil.java
@@ -9,7 +9,7 @@ public class DbUtil {
     // MySQL 8.0 以上版本 - JDBC 驱动名及数据库 URL
     private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String DB_URL =
-            "jdbc:mysql://localhost:3306/busyfish?useSSL=false&serverTimezone=UTC";
+            "jdbc:mysql://localhost:3306/busyfish?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
 
     // 数据库的用户名与密码，需要根据自己的设置
     private static final String USER = "root";
@@ -60,13 +60,41 @@ public class DbUtil {
     }
 
     /**
-     * TODO 检测用户名和密码是否匹配
+     * 检测用户名和密码是否匹配
      * @param account_id 用户名
      * @param account_password 密码
      * @return true代表匹配，false代表不匹配
      */
-    public static boolean idMatch(String account_id, String account_password) {
-        return true;
+    public static boolean idMatch(String account_id, String account_password) throws SQLException {
+        // 取得连接
+        Connection conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+
+        // sql查询语句
+        String sql = "select account_password from accounts where account_id=?";
+
+        // 创建PreparedStatement对象
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ps.setString(1, account_id);
+
+        ResultSet result = null;
+        String passwordQueried = null;
+        try {
+            // executeQuery()函数执行查询操作
+            result = ps.executeQuery();
+            // result指向最后一行（查询到的数据最多只有一行）
+            result.last();
+            if(result.getRow() == 1) { // 如果当前的行数为1
+                passwordQueried = result.getString(1);
+            }
+            else return false; // 如果行数为0则账号不存在
+        } catch(SQLException e) {
+            e.printStackTrace();
+        } finally {
+            conn.close();
+            ps.close();
+            result.close();
+        }
+        return passwordQueried.equals(account_password);
     }
 
 }
