54d314feaa7abe30079f75b79af1c81575ebac4c
Qie-Dan <13799658498@163.com>
2020-04-11 22:26:15 +0800

    add UserThread.java

--- src/server/threads/LoginThread.java
+++ src/server/threads/LoginThread.java
@@ -11,10 +11,12 @@ import java.sql.SQLException;
  */
 public class LoginThread extends Thread {
     private Socket client;
+    public static final String SUCCESS_MSG = "Login succeeded";
+    public static final String FAIL_MSG = "Login failed";
 
     /**
      * 构造方法
-     * @param client 输入账号密码的socket
+     * @param client 客户端socket
      */
     public LoginThread(Socket client) {
         // 开启新线程
@@ -24,30 +26,35 @@ public class LoginThread extends Thread {
     // 启动线程，响应客户端请求
     @Override
     public void run() {
+        BufferedReader br = null;
+        BufferedWriter bw = null;
+        PrintWriter pw = null;
         try {
 
-            BufferedReader br = new BufferedReader(new InputStreamReader(
-                    client.getInputStream()
-            ));
+            br = new BufferedReader(new InputStreamReader(
+                    client.getInputStream()));
 
-            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(
-                    client.getOutputStream()
-            ));
-            PrintWriter pw = new PrintWriter(bw, true);
+            bw = new BufferedWriter(new OutputStreamWriter(
+                    client.getOutputStream()));
+
+            pw = new PrintWriter(bw, true);
 
             // 读取客户端传来的用户名
             String account_id = br.readLine();
             // 读取客户端传来的密码
             String password = br.readLine();
 
-            // TODO 让用户名和密码比对
+            // 让用户名和密码比对
             if(DbUtil.idMatch(account_id, password)) {
-
+                // 密码正确，则发送登录成功提示，并开启一个新线程
+                pw.println(SUCCESS_MSG);
+                new UserThread(client).start();
+            } else {
+                pw.println(FAIL_MSG);
             }
-
-
         } catch (IOException | SQLException e) {
             e.printStackTrace();
+            pw.println(FAIL_MSG);
         }
     }
 }
---
+++ src/server/threads/UserThread.java
@@ -0,0 +1,22 @@
+package server.threads;
+
+import java.net.Socket;
+
+/**
+ * 正式服务线程，处理一系列用户请求
+ */
+public class UserThread extends Thread {
+
+    /**
+     * 构造方法
+     * @param client 客户端的socket
+     */
+    public UserThread(Socket client) {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
