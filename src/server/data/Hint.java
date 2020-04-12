360d5e85e80b1603ca05b220c4ebf5c1a1fc5007
Qie-Dan <13799658498@163.com>
2020-04-12 19:01:17 +0800

    add Hint.java

---
+++ src/server/data/Hint.java
@@ -0,0 +1,19 @@
+package server.data;
+
+/**
+ * 该类用于发送请求成功的提示信息，如注册成功与否等
+ */
+public class Hint {
+    private boolean success;
+    public Hint(boolean success) {
+        this.success = success;
+    }
+
+    /**
+     * 得知是否成功提示
+     * @return 若为true，则相应的请求为成功
+     */
+    public boolean isSuccess() {
+        return this.success;
+    }
+}
