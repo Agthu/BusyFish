c3f8f21f2f3a86b3aece83126a0e04f154ecf46f
Qie-Dan <13799658498@163.com>
2020-04-12 09:36:31 +0800

    add User.java

---
+++ src/server/data/User.java
@@ -0,0 +1,19 @@
+package server.data;
+
+/**
+ * 用户类，用于传输对象
+ */
+public class User {
+    private String id;
+    private String name;
+    private String password;
+
+    /**
+     * 构造方法，用于登录时传输对象
+     * @param id 用户名
+     * @param password 密码
+     */
+    public User(String id, String password) {
+
+    }
+}
