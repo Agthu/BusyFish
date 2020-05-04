f92dfa8654f8fa600136c82b24c8a99af1ea542a
zz12-123 <62467168+zz12-123@users.noreply.github.com>
2020-05-04 23:08:57 +0800

    Update BuyProductFrame.java

--- src/client/ui/BuyProductFrame.java
+++ src/client/ui/BuyProductFrame.java
@@ -1,111 +1,133 @@
 package client.ui;
-import java.awt.Font;
+import javax.swing.JFrame;
 import javax.swing.UIManager;
-
-public class ViewMainFrame extends javax.swing.JFrame {
-
-	/** Creates new form ViewMainFrm */
-	public ViewMainFrame() {
-		 //改变系统默认字体
-		Font font = new Font("Dialog", Font.PLAIN, 18);
-		java.util.Enumeration keys = UIManager.getDefaults().keys();
-		while (keys.hasMoreElements()) {
-			Object key = keys.nextElement();
-			Object value = UIManager.get(key);
-			if (value instanceof javax.swing.plaf.FontUIResource) {
-				UIManager.put(key, font);
-			}
-		}
+import javax.swing.table.DefaultTableModel;
+public class BuyProductFrame extends javax.swing.JFrame {
+	public BuyProductFrame() {
 		initComponents();
-		//设置frame居中显示
-		this.setLocationRelativeTo(null);
+		this.setLocationRelativeTo(null);                  //居中显示 
 	}
-
-	
 	private void initComponents() {
-
-		jLabel1 = new javax.swing.JLabel();
-		jb_buyer = new javax.swing.JButton();
-		jb_seller = new javax.swing.JButton();
-		jButton = new javax.swing.JButton();
-
-		setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-		jLabel1.setFont(new java.awt.Font("宋体", Font.BOLD, 40));
-		jLabel1.setText("BusyFish");
-
-		jb_buyer.setText("我是买家");
-		jb_buyer.addActionListener(new java.awt.event.ActionListener() {                //设置 我是买家监听器
-			public void actionPerformed(java.awt.event.ActionEvent evt) {              
-				jb_BuyProductActionPerformed(evt);
-			}
-		});
-
-		jb_seller.setText("我是卖家"); 
-		jb_seller.addActionListener(new java.awt.event.ActionListener() {                // 设置 我是卖家监听器
-			public void actionPerformed(java.awt.event.ActionEvent evt) {
-				jb_AddProductActionPerformed(evt);
-			}
-		});
-
-		jButton.setText("我的消息");
-		
-		jButton.addActionListener(new java.awt.event.ActionListener() {
-			public void actionPerformed(java.awt.event.ActionEvent evt) {                 //  设置 我的消息 监听器
-				jb_MessagetActionPerformed(evt);
-			}
-		});
-		javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-		getContentPane().setLayout(layout);
-		layout.setHorizontalGroup(layout.createParallelGroup(   //创建GroupLayout的水平连续组
-				javax.swing.GroupLayout.Alignment.LEADING).addGroup(  //指示元素从原点对齐
-				layout.createSequentialGroup().addContainerGap(111,Short.MAX_VALUE).
-						addComponent(jLabel1,javax.swing.GroupLayout.PREFERRED_SIZE, 300,javax.swing.GroupLayout.PREFERRED_SIZE)
-						/*
-						 * 控制JLabel水平连续组的对应的间隙
-						 */
-					    .addContainerGap()).addGroup(
-				layout.createSequentialGroup().addGap(27, 27, 27).addComponent(jb_buyer).
-				addGap(57, 57, 57).addComponent(jb_seller).
-                addGap(57,57,57).addComponent(jButton).addGap(22, 22,22))); //控制JButton水平连续组对应的间隙
-		layout
-				.setVerticalGroup(layout                             //创建GroupLayout的垂直连续组
-						.createParallelGroup(
-								javax.swing.GroupLayout.Alignment.LEADING)
-						.addGroup(
-								layout
-										.createSequentialGroup()
-										.addGap(29, 29, 29)//控制JLabel和顶端距离
-				        .addComponent(jLabel1,javax.swing.GroupLayout.PREFERRED_SIZE,130,javax.swing.GroupLayout.PREFERRED_SIZE)
-										.addGap(27, 27, 27)//控制JLabel和JButton距离
-										.addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-														.addComponent(jb_buyer)
-														.addComponent(jb_seller)
-														.addComponent(jButton))
-										.addContainerGap(87, Short.MAX_VALUE))); // 控制JButton和底部距离
-
-		pack();
+jPanel = new javax.swing.JPanel();
+jLabel1 = new javax.swing.JLabel();
+s_productNameTxt = new javax.swing.JTextField();
+jLabel2 = new javax.swing.JLabel();
+jb_search = new javax.swing.JButton();
+jScrollPane1 = new javax.swing.JScrollPane();
+productTable = new javax.swing.JTable();
+jb_cost = new javax.swing.JButton();
+jPanel.setBorder(javax.swing.BorderFactory.createTitledBorder("交易广场"));    //设置标题为 交易广场  
+jLabel1.setText("搜索商品");
+jLabel2.setText("商品类别");
+jb_search.setIcon(new javax.swing.ImageIcon("./images\\search.png")); //  设置搜索按钮的图片
+jb_search.setText("搜索");
+jb_search.addActionListener(new java.awt.event.ActionListener() {          //设置搜索按钮的监听器
+public void actionPerformed(java.awt.event.ActionEvent evt) {                
+jb_searchActionPerformed(evt);
+}
+});
+productTable.setModel(new javax.swing.table.DefaultTableModel(                  //创建商品列表的表格
+	new Object [][] {	
+	},
+	new String [] {
+		 "商品名称", "发布人", "商品描述", "商品价格", 
 	}
-	public static void main(String args[]) {
-		java.awt.EventQueue.invokeLater(new Runnable() {                      //
+) {
+	boolean[] canEdit = new boolean [] {
+		 false, false, false, false,
+	};
+	public boolean isCellEditable(int rowIndex, int columnIndex) {
+		return canEdit [columnIndex];
+	}
+});
+productTable.addMouseListener(new java.awt.event.MouseAdapter() {              //设置点击表格的监听器
+public void mousePressed(java.awt.event.MouseEvent evt) {              
+productTableMousePressed(evt);
+}
+});
+jScrollPane1.setViewportView(productTable);            //使表格可以滚动
+
+jb_cost.setIcon(new javax.swing.ImageIcon("./images\\shopcart.png")); //   添加购买按钮的图片
+jb_cost.setText("购买");
+jb_cost.addActionListener(new java.awt.event.ActionListener() {
+public void actionPerformed(java.awt.event.ActionEvent evt) {
+jb_costActionPerformed(evt);                                             //设置购买按钮监听器
+}
+});
+
+
+javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel);
+jPanel.setLayout(jPanel1Layout);
+jPanel1Layout.setHorizontalGroup(                          //创建水平连续组
+jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+.addGroup(jPanel1Layout.createSequentialGroup()
+.addGap(20, 20, 20)
+.addComponent(jLabel1)        //商品名称按钮
+.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+.addComponent(s_productNameTxt, javax.swing.GroupLayout.PREFERRED_SIZE, 250, javax.swing.GroupLayout.PREFERRED_SIZE)//设置商品名称搜索框的长度
+.addGap(18, 18, 18)
+.addComponent(jb_cost)
+.addGap(18, 18, 18)
+.addComponent(jb_search)          //设置搜索按钮
+.addGap(50, 50, 50))
+.addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 792, Short.MAX_VALUE)
+);
+jPanel1Layout.setVerticalGroup(         //创建垂直连续组
+jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+.addGroup(jPanel1Layout.createSequentialGroup()
+.addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+.addComponent(jLabel1)
+.addComponent(s_productNameTxt, 20,20,20)		 //设置商品搜索框的大小
+.addComponent(jb_search))                               
+.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)       
+.addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 250, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            					//设置表格高度
+.addComponent(jb_cost)                        	// 
+.addContainerGap(30, Short.MAX_VALUE)));        // 设置购买按钮下间隙
+
+javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+layout.setHorizontalGroup(
+layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+.addComponent(jPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+);
+layout.setVerticalGroup(
+layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+.addComponent(jPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+)                       //添加JPanel
+;
+pack();
+}
+	/*
+	 * 设置布局
+	 */
+	
+	
+/*	private void jb_searchActionPerformed(java.awt.event.ActionEvent evt) {
+		String productName = this.s_productNameTxt.getText();
+		Product product = new Product(productName);
+		this.fillTable(product);                            // 设置点击 搜索 后的触发事件 (根据商品名字查找商品)
+	}*/public static void main(String args[]) {
+		java.awt.EventQueue.invokeLater(new Runnable() {             
 			public void run() {
-				new ViewMainFrame().setVisible(true);
+				new BuyProductFrame().setVisible(true);
 			}
-		});
+		});}
+	
+	
+	private void jb_costActionPerformed(java.awt.event.ActionEvent evt) {             //设置点击 购买 后的触发事件
+		
 	}
-	private void jb_BuyProductActionPerformed(java.awt.event.ActionEvent evt) {             //设置点击 我是买家 后的触发事件
-		new BuyProductFrame().setVisible(true);                                           
-	}                                                             
-	private void jb_AddProductActionPerformed(java.awt.event.ActionEvent evt) {             //设置点击 我是卖家 后的触发事件
-		new AddProductFrame().setVisible(true); 
-	}
-	private void jb_MessagetActionPerformed(java.awt.event.ActionEvent evt) {             //设置点击 我的消息 后的触发事件
-		new MessageFrame().setVisible(true); 
+	private void productTableMousePressed(java.awt.event.MouseEvent evt) {             //设置点击 商品表格 后的触发事件
+		
 	}
 	
-	private javax.swing.JButton jButton;
 	private javax.swing.JLabel jLabel1;
-	private javax.swing.JButton jb_seller;
-	private javax.swing.JButton jb_buyer;
+	private javax.swing.JLabel jLabel2;
+	private javax.swing.JPanel jPanel;
+	private javax.swing.JScrollPane jScrollPane1;
+	private javax.swing.JButton jb_cost;
+	private javax.swing.JButton jb_search;
+	private javax.swing.JTable productTable;
+	private javax.swing.JTextField s_productNameTxt;
 
 }
