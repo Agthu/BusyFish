da0031cf4d15b6b6ce8c48c55a303cdafc47e7c5
zz12-123 <62467168+zz12-123@users.noreply.github.com>
2020-05-24 23:35:43 +0800

    Update MessageFrame.java

--- src/client/ui/MessageFrame.java
+++ src/client/ui/MessageFrame.java
@@ -1,14 +1,19 @@
 package client.ui;
 import javax.swing.*;
+
+import client.UserClient;
+
 import java.awt.*;
 import java.awt.event.*;
 public class MessageFrame extends javax.swing.JFrame {
     public static void main(String[] args) {
-        MessageFrame f = new MessageFrame();
+        MessageFrame f = new MessageFrame(user);
     }
-
+    private  UserClient user;
     
-    public MessageFrame() {
+    public MessageFrame(UserClient user) {
+		// 当前用户
+		this.user = user;
 		JFrame chat_frame = new JFrame();
 		chat_frame.setSize(450,480);                              //设置大小
 		chat_frame.setLocationRelativeTo(null);
@@ -21,7 +26,10 @@ public class MessageFrame extends javax.swing.JFrame {
 		chat_frame.add(input_area);
 		chat_frame.add(send);
 		chat_frame.setVisible(true);
-		
+		 show_area.setEditable(false);
+	       /*
+	        * 设置不可编辑 
+	        */
 		
 		
 	send.addActionListener(new java.awt.event.ActionListener() {                //设置 发送消息监听器
