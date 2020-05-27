5ac4a2aeb5b79df00e73e45167b2888150657879
zz12-123 <62467168+zz12-123@users.noreply.github.com>
2020-05-27 20:26:16 +0800

    Update MessageFrame.java

--- src/client/ui/MessageFrame.java
+++ src/client/ui/MessageFrame.java
@@ -1,13 +1,11 @@
 package client.ui;
 import javax.swing.*;
-
 import client.UserClient;
-
 import java.awt.*;
 import java.awt.event.*;
 public class MessageFrame extends javax.swing.JFrame {
     public static void main(String[] args) {
-        MessageFrame f = new MessageFrame(user);
+        MessageFrame f = new MessageFrame(null);
     }
     private  UserClient user;
     
@@ -15,23 +13,26 @@ public class MessageFrame extends javax.swing.JFrame {
 		// 当前用户
 		this.user = user;
 		JFrame chat_frame = new JFrame();
-		chat_frame.setSize(450,480);                              //设置大小
+		chat_frame.setSize(450,520);                              //设置大小
 		chat_frame.setLocationRelativeTo(null);
 		chat_frame.setLayout(new FlowLayout());
  
 		JTextArea show_area = new JTextArea(10,28);                //设置聊天框大小
 		JTextArea input_area = new JTextArea(5,28);                // 设置输入框大小
-		JButton send = new JButton("发送消息");	         	
+		JButton send = new JButton("发送消息");	  
+		JLabel id=new JLabel(" 输入接收人id:");
+		JTextField id_input=new JTextField(15);                     // 设置输入接收人id的输入框
 		chat_frame.add(show_area);
 		chat_frame.add(input_area);
+		chat_frame.add(id);
+		chat_frame.add(id_input);		
 		chat_frame.add(send);
 		chat_frame.setVisible(true);
 		 show_area.setEditable(false);
 	       /*
 	        * 设置不可编辑 
 	        */
-		
-		
+	
 	send.addActionListener(new java.awt.event.ActionListener() {                //设置 发送消息监听器
 			public void actionPerformed(java.awt.event.ActionEvent evt) {              
 				sendActionPerformed(evt);
